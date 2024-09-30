39675be3e950d8711b1ce1c95a5e1d61d80703b3
venuscyr <venus.amako@epitech.digital>
2024-09-30 15:46:08 +0200

    ex_08.js

---
+++ ex_08.js
@@ -0,0 +1,12 @@
+if (breadCount >= 2 && hamCount >= 1 && tunaCount >= 1){
+displayThisText("i can make a royal sandwich!");
+}
+else if (breadCount >= 2 && hamCount >= 1){
+displayThisText("i can make a ham sandwich!");
+}
+else if (breadCount >= 2 && tunaCount >= 1){
+displayThisText("i can make a tuna sandwich!");
+}
+else{
+displayThisText("i'd rather be fasting tonight!");
+}
