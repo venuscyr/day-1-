1ce2cf20cd33ecc24c360e11d3140c3eeb124069
venuscyr <venus.amako@epitech.digital>
2024-09-30 14:08:34 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,7 @@
+let firstname = "John"
+let lastname = "Doe"
+let cardCode = "1111 2222 3333 4444"
+let expiryDateMonth = 21;
+let expiryDateYear = 2042;
+let computedBankInfo= firstname + ","+  lastname + "," + cardCode + "," + expiryDateMonth + "/"  +expiryDateYear
+displayInCard(computedBankInfo)
