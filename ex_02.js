80ac9446534fc7cc89c6d6e74e83b6b88b086807
venuscyr <venus.amako@epitech.digital>
2024-09-30 11:23:55 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,10 @@
+let integer = "42<br/>"
+document.write(integer)
+let message ="null<br/>"
+document.write(message)
+let bool ="true<br/>"
+document.write(bool)
+let string ="forty two<br/>"
+document.write(string)
+let float ="42,42<br/>"
+document.write(float)
